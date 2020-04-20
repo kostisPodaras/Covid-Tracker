00c7c638a5c47543e9cec9d32f765a1a9479e6e1
Podaras <PodarasK@kotsovolos.gr>
2020-04-20 23:12:08 +0300

    testing deployment

--- src/redux/countries/countriesAction.js
+++ src/redux/countries/countriesAction.js
@@ -21,7 +21,6 @@ const fetchCountriesFailure = () => ({
 const fetchCountries = () => {
   return async (dispatch) => {
     dispatch(fetchCountriesRequest());
-    // dispatch(fetchCountriesSucess);
     const request = await fetch(COUNTRIES_API);
     const { countries } = await request.json();
     dispatch(fetchCountriesSucess(countries));
