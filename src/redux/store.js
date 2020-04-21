c789d998b6faf0143ebd351343c0b9054c556b9a
Podaras <PodarasK@kotsovolos.gr>
2020-04-21 22:25:50 +0300

    remove redux devtools for production

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,12 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./rootReducer";
-const middleware = [thunk];
 
 const store = createStore(
   rootReducer,
   compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(thunk)
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
