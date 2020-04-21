f7d4148889d07241f59ed05980104233355a7d89
Podaras <PodarasK@kotsovolos.gr>
2020-04-21 22:20:08 +0300

    tryed new way to test apply middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,12 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./rootReducer";
+const middleware = [thunk];
 
 const store = createStore(
   rootReducer,
   compose(
-    applyMiddleware(thunk),
+    applyMiddleware(...middleware),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
