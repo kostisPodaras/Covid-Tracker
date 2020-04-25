e9247c01afbc542de4c0dc75fa4a8611146d651d
Podaras <PodarasK@kotsovolos.gr>
2020-04-25 17:08:31 +0300

    minor style changes

--- src/components/chart/styles.js
+++ src/components/chart/styles.js
@@ -2,7 +2,7 @@ const styles = () => ({
   container: {
     width: "90%",
     height: "40vh",
-    marginBottom: 20,
+    marginBottom: 60,
 
     "@media (max-width: 400px)": {
       width: "100%",
